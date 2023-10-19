e4767093421c47be60691eb83a9952e628b9ff68
Chenyang Wei <ChenyangWei.CWei@gmail.com>
2023-10-19 12:40:33 -0400

    Create an R script for joining the rotated/extended transects and the corresponding segments.

---
+++ Scripts/01_Transect_Segment_Joining.R
@@ -0,0 +1,13 @@
+# Introduction. -----------------------------------------------------------
+
+# 1) Join the rotated/extended transects with
+#   the corresponding segments.
+
+# Updated: 10/19/2023.
+
+
+# 0) Setup. ---------------------------------------------------------------
+
+# Load Packages.
+library(tidyverse)
+library(sf)
