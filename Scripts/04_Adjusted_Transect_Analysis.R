5df38c11c0457965dfe8711169adb69a872e1cd1
Chenyang Wei <ChenyangWei.CWei@gmail.com>
2023-11-03 16:51:37 -0400

    Analyze the rotated transects.

--- Scripts/04_Adjusted_Transect_Analysis.R
+++ Scripts/04_Adjusted_Transect_Analysis.R
@@ -39,7 +39,7 @@ extended_SHP <- st_read(
   stringsAsFactors = TRUE)
 
 nrow(extended_SHP) # 45872.
-head(extended_SHP)
+# head(extended_SHP)
 
 # Load the NDVI/VCH differences of the rotated transects.
 folderName <- "Rotation"
@@ -56,7 +56,7 @@ rotated_SHP <- st_read(
   stringsAsFactors = TRUE)
 
 nrow(rotated_SHP) # 66345.
-head(rotated_SHP)
+# head(rotated_SHP)
 
 
 # 2) Check the extended transects. ----------------------------------------
@@ -75,6 +75,23 @@ extendedNDVI_DF <-
   extended_DF %>% 
   select(starts_with("NDVI"))
 
+# Median.
+extendedVCH_DF %>% 
+  summarise(across(where(is.numeric), 
+                   .fns = list(
+                     median = median
+                   )))%>%
+  pivot_longer(everything()) %>% 
+  arrange(desc(value))
+
+extendedNDVI_DF %>% 
+  summarise(across(where(is.numeric), 
+                   .fns = list(
+                     median = median
+                   )))%>%
+  pivot_longer(everything()) %>% 
+  arrange(desc(value))
+
 # Summary.
 extendedVCH_DF %>% 
   summary()
@@ -82,19 +99,45 @@ extendedVCH_DF %>%
 extendedNDVI_DF %>% 
   summary()
 
-# Median.
-extendedVCH_DF %>% 
-  summarise(across(where(is.numeric), 
-                   .fns = list(
-                     median = median
-                     )))%>%
-  pivot_longer(everything())
 
-extendedNDVI_DF %>% 
+# 3) Check the rotated transects. -----------------------------------------
+
+rotated_DF <- 
+  rotated_SHP %>% 
+  st_drop_geometry()
+
+# VCH difference.
+rotatedVCH_DF <- 
+  rotated_DF %>% 
+  select(starts_with("VCH"))
+
+# NDVI difference.
+rotatedNDVI_DF <- 
+  rotated_DF %>% 
+  select(starts_with("NDVI"))
+
+# Median.
+rotatedVCH_DF %>% 
   summarise(across(where(is.numeric), 
                    .fns = list(
                      median = median
-                     )))%>%
-  pivot_longer(everything())
+                   )))%>%
+  pivot_longer(everything()) %>% 
+  arrange(desc(value))
+
+rotatedNDVI_DF %>% 
+  summarise(across(where(is.numeric), 
+                   .fns = list(
+                     median = median
+                   )))%>%
+  pivot_longer(everything()) %>% 
+  arrange(desc(value))
+
+# Summary.
+rotatedVCH_DF %>% 
+  summary()
+
+rotatedNDVI_DF %>% 
+  summary()
 
 
